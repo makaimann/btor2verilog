a1c99ee0f541beefc073c25ffba7f706b834ffe9
Makai Mann <makaim@stanford.edu>
2020-04-20 01:30:18 -0700

    Start mapping operators

--- btor2verilog.cpp
+++ btor2verilog.cpp
@@ -5,6 +5,82 @@ using namespace std;
 namespace btor2verilog
 {
 
+const unordered_map<Btor2Tag, string> bvopmap({
+    { BTOR2_TAG_add, "+" },
+    { BTOR2_TAG_and, "&" },
+    // { BTOR2_TAG_bad, },
+    //{ BTOR2_TAG_concat, Concat },
+    //{ BTOR2_TAG_const, },
+    //{ BTOR2_TAG_constraint, },
+    //{ BTOR2_TAG_constd, },
+    //{ BTOR2_TAG_consth, },
+    //{ BTOR2_TAG_dec, },
+    // { BTOR2_TAG_eq, BVComp }, // handled this specially, because could also
+    // have array arguments
+    //{ BTOR2_TAG_fair, },
+    { BTOR2_TAG_iff, "==" },
+    // { BTOR2_TAG_implies, Implies }, // handle specially (needs to work with
+    // boolector AND other solvers), gets complicated with bools and BV(1) are
+    // aliased
+    //{ BTOR2_TAG_inc, },
+    //{ BTOR2_TAG_init, },
+    //{ BTOR2_TAG_input, },
+    //{ BTOR2_TAG_ite, Ite },
+    //{ BTOR2_TAG_justice, },
+    { BTOR2_TAG_mul, "*" },
+    //{ BTOR2_TAG_nand, BVNand },
+    { BTOR2_TAG_neq, "!=" },
+    { BTOR2_TAG_neg, BVNeg },
+    //{ BTOR2_TAG_next, },
+    //{ BTOR2_TAG_nor, BVNor },
+    { BTOR2_TAG_not, "~" },
+    //{ BTOR2_TAG_one, },
+    //{ BTOR2_TAG_ones, },
+    { BTOR2_TAG_or, "|" },
+    //{ BTOR2_TAG_output, },
+    // { BTOR2_TAG_read, Select }, // handle specially -- make sure it's casted
+    // to bv
+    { BTOR2_TAG_redand, "&"},
+    { BTOR2_TAG_redor, "|"},
+    { BTOR2_TAG_redxor, "^"},
+    // { BTOR2_TAG_rol, },
+    // { BTOR2_TAG_ror, },
+    //{ BTOR2_TAG_saddo, },
+    //{ BTOR2_TAG_sdiv, BVSdiv },
+    //{ BTOR2_TAG_sdivo, },
+    //{ BTOR2_TAG_sext, },
+    //{ BTOR2_TAG_sgt, BVSgt },
+    //{ BTOR2_TAG_sgte, BVSge },
+    //{ BTOR2_TAG_slice, },
+    { BTOR2_TAG_sll, "<<" },
+    // { BTOR2_TAG_slt, BVSlt },
+    // { BTOR2_TAG_slte, BVSle },
+    //{ BTOR2_TAG_sort, },
+    // { BTOR2_TAG_smod, BVSmod },
+    //{ BTOR2_TAG_smulo, },
+    { BTOR2_TAG_sra, ">>>" },
+    // { BTOR2_TAG_srem, BVSrem },
+    { BTOR2_TAG_srl, ">>" },
+    //{ BTOR2_TAG_ssubo, },
+    //{ BTOR2_TAG_state, },
+    { BTOR2_TAG_sub, "-" },
+    //{ BTOR2_TAG_uaddo, },
+    { BTOR2_TAG_udiv, "/" },
+    //{ BTOR2_TAG_uext, },
+    { BTOR2_TAG_ugt, ">" },
+    { BTOR2_TAG_ugte, ">=" },
+    { BTOR2_TAG_ult, "<" },
+    { BTOR2_TAG_ulte, "<=" },
+    //{ BTOR2_TAG_umulo, },
+    { BTOR2_TAG_urem,  },
+    //{ BTOR2_TAG_usubo, },
+    //{ BTOR2_TAG_write, Store }, // handle specially -- make sure it's casted
+    //to bv
+    { BTOR2_TAG_xnor, BVXnor },
+    { BTOR2_TAG_xor, BVXor },
+    //{ BTOR2_TAG_zero, }
+});
+
 void Btor2Verilog::initialize()
 {
   err_ = "";
